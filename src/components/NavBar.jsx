eceebd1771b292877197ad257c88ad94a49e6407
atharva <atharvamk6@gmail.com>
2024-07-27 22:53:48 +0530

    Resume Download feature added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Resume from '../assets/Resume.pdf'
 
 const NavBar = () => {
   return (
-    <nav className="w-screen flex justify-evenly bg-slate-600 ">
+    <nav className="w-screen z-40 flex justify-evenly bg-slate-600 ">
         <a>Projects</a>
         <a>Leetcode</a>
         <a>GitHub</a>
         <a>Insta</a>
         <a href="" className='nav-link'>Contact Me</a>
+        <a href={Resume} download="AtharvaKulkarniResume" className='nav-link'>Resume</a>
     </nav>
   )
 }
