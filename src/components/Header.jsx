ac89b99ae3efd61f04642e5c1c64f6e01c482629
atharva <atharvamk6@gmail.com>
2024-08-04 02:01:07 +0530

    Header changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,19 @@ const Header = () => {
   return (
     <>
     <header className="header">
+    <motion.h1
+        className="blur-card-motion"
+        initial={{  z : 20 }}
+        animate={{  z : 15 }}
+        transition={{ duration: 1 }}
+        whileTap={{ scale: 0.95 }} 
+      >
+      <div children className='blur-card'></div>
+      </motion.h1>
       <motion.h1
         className="header-title"
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100, z:10 }}
+        animate={{ opacity: 1, x: 0 , z : 20}}
         transition={{ duration: 1 }}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }} 
@@ -17,8 +26,8 @@ const Header = () => {
       </motion.h1>
       <motion.h1
         className="header-title"
-        initial={{ opacity: 0, x: 100 }}
-        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 , z: 10}}
+        animate={{ opacity: 1, x: 0 , z : 20}}
         transition={{ duration: 1 }}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }} 
