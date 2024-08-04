e02860e29eca0946a072c323ed871a9a77925ea0
atharva <atharvamk6@gmail.com>
2024-08-04 13:22:30 +0530

    Favicon saved

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { MdDownload } from 'react-icons/md';
+import '../assets/Resume.pdf';
 
 
 const NavBar = () => {
@@ -12,12 +13,14 @@ const NavBar = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <li className="navbar-item"><a href="#portfolio-summary">Summary</a></li>
         <li className="navbar-item"><a href="#projects">Projects</a></li>
-        <li className="navbar-item"><a href="#skills">Skills</a></li>
-        <li className="navbar-item"><a href="#experience">Experience</a></li>
-        <li className="navbar-item"><a href="#contact">Contact</a></li>
-        <li className="navbar-item"><button className='flex items-center' >Resume <MdDownload /></button></li>
+        <li className="navbar-item"><a href="https://linkedin.com/in/atharva-kulkarni-cs06">Ln</a></li>
+        <li className="navbar-item"><a href="https://leetcode.com/u/AtharvaKulkarni612">LT</a></li>
+        <li className="navbar-item"><a href="https://github.com/athrvdev">GitHUb</a></li>
+        <li className="navbar-item"><button 
+        // onClick={() => window.open(Resume)}  
+        // download={Resume} fileName="Resume"
+        className='flex items-center' >Resume <MdDownload /></button></li>
       </motion.ul>
     </nav>
   );
