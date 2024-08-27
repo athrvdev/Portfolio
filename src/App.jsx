d297ea9886f7aa27a9156c45ee17658b2989e7a0
Atharva Kulkarni <atharvamk6@gmail.com>
2024-08-28 00:44:26 +0530

    initial Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import About from './components/About'; 
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import ProfileCard from './components/ProfileCard'
 
 const App = () => {
   return (
-    <div>Development in progress...</div>
-  )
-}
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ProfileCard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
