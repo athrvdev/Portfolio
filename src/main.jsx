d8a415fe674517daa18560d1e1b45c9f3a1f7125
Atharva Kulkarni <atharvamk6@gmail.com>
2024-08-28 00:46:33 +0530

    react-router-dom BrowserRouter configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
